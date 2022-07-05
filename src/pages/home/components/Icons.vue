654382a483dfe1c6d050dfef384d4e228d04d530
18583696428 <ls583561608>
2022-07-06 01:11:01 +0800

    add recommand

--- src/pages/home/components/Icons.vue
+++ src/pages/home/components/Icons.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="icons">
-    <swiper :options="swiperOption" ref="mySwiper">
+    <swiper ref="mySwiper">
       <swiper-slide v-for="(page, index) of pages" :key="index">
         <div class="icon" v-for="item of page" :key="item.id">
           <div class="icon-img">
@@ -103,33 +103,35 @@ export default {
     width: 100%
     height: 0
     padding-bottom: 50%
-  .icon
-    position: relative
-    overflow: hidden
-    float: left
-    width: 25%
-    height: 0
-    padding-bottom: 25%
-    .icon-img
-      position: absolute
-      top: 0
-      left: 0
-      right: 0
-      bottom: .44rem
-      box-sizing: border-box
-      padding: .1rem
-      .icon-img-content
-        display: block
-        margin: 0 auto
-        height: 100%
-    .icon-desc
-      position: absolute
-      color: $darkTextColor
-      left: 0
-      right: 0
-      bottom: 0
-      height: .44rem
-      line-height: .44rem
-      text-align: center
-      ellipsis()
+  .icons
+    margin-top: .2rem
+    .icon
+      position: relative
+      overflow: hidden
+      float: left
+      width: 25%
+      height: 0
+      padding-bottom: 25%
+      .icon-img
+        position: absolute
+        top: 0
+        left: 0
+        right: 0
+        bottom: .44rem
+        box-sizing: border-box
+        padding: .1rem
+        .icon-img-content
+          display: block
+          margin: 0 auto
+          height: 100%
+      .icon-desc
+        position: absolute
+        color: $darkTextColor
+        left: 0
+        right: 0
+        bottom: 0
+        height: .44rem
+        line-height: .44rem
+        text-align: center
+        ellipsis()
 </style>
